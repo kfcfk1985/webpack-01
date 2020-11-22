29f94be29702411a7a2c0536de97d303b0ce2327
ldf <kfcfk1985@163.com>
2020-11-22 20:22:35 +0800

    修改webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name]-[hash:8].js"
+    },
+    mode: 'development',
+    plugins: [
+        /**
+         * All files inside webpack's output.path directory will be removed once, but the
+         * directory itself will not be. If using webpack 4+'s default configuration,
+         * everything under <PROJECT_DIR>/dist/ will be removed.
+         * Use cleanOnceBeforeBuildPatterns to override this behavior.
+         *
+         * During rebuilds, all webpack assets that are not used anymore
+         * will be removed automatically.
+         *
+         * See `Options and Defaults` for information
+         */
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, ],
+    },
+}
